291fd1276f03056a0081df9ebf9d5739da6fda12
koktear <tian@tiandeiMac.local>
2015-06-17 09:58:59 +0800

    label 文字颜色 点击事件

---
+++ WPAttributedMarkupDemo/WPAttributedMarkupDemo/ViewController.h
@@ -0,0 +1,24 @@
+//
+//  ViewController.h
+//  WPAttributedMarkupDemo
+//
+//  Created by Nigel Grange on 15/10/2014.
+//  Copyright (c) 2014 Nigel Grange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class WPHotspotLabel;
+
+@interface ViewController : UIViewController 
+
+@property (weak, nonatomic) IBOutlet UILabel *label1;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet WPHotspotLabel *label3;
+
+@end
+
+
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com 
